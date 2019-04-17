9fc65b057d18d199464f33b17d4e51cde98a7b09
mike <100000356@qq.com>
2019-04-18 07:56:27 +0800

    add

--- server/config/db.php
+++ server/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=47.92.6.86;dbname=ybjc',
     'username' => 'user',
-    'password' => 'storecode8888',
+    'password' => 'storecode88881',
     'charset' => 'utf8',
     'tablePrefix'=>'ssc_',
     // Schema cache options (for production environment)
